afe917dadf9ba3f1119d9d9ebe0b949ad8fc9311
Sacha Percot-Tétu <sacha@percot-tetu.fr>
2012-06-15 17:04:59 +0200

    Improved the NavigationBar performance by precalculating and storing the QPixMap inside each Node

--- GenericInterface/Services/Node.h
+++ GenericInterface/Services/Node.h
@@ -6,28 +6,34 @@
 #include <QList>
 #include <QMdiSubWindow>
 #include <QMetaType>
+#include <QPixmap>
 
 #include <Image.h>
 
 
 namespace genericinterface
 {
+    QImage convertImage(const imagein::Image* image);
     struct NodeId {
       public:
         inline NodeId(const imagein::Image* id = NULL) : _id(id) {}
         inline bool operator==(const NodeId& other) { return _id==other._id; }
         inline bool operator<(const NodeId& other) const { return _id<other._id; }
+        inline bool isValid() { return _id != NULL;}
       private:
         const imagein::Image* _id;    
     };
     
     struct Node {
         inline Node() : image(NULL), path("") {}
-        inline Node(const imagein::Image* img, QString path_) : image(img), path(path_) {}
+        inline Node(const imagein::Image* img, QString path_) : image(img), path(path_), pixmap(QPixmap::fromImage(convertImage(img))) {}
+        inline Node(QPixmap pixmap_, const imagein::Image* img, QString path_) : image(img), path(path_), pixmap(pixmap_) {}
         NodeId getId() const { return image; }
+        inline bool isValid() { return image != NULL;}
         const imagein::Image* image;
         QString path;
         QList<QMdiSubWindow*> windows;
+        QPixmap pixmap;
         ~Node() {
         }
     };
