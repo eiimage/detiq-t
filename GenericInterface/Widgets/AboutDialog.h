e73f6968e5d3222e7815b61d3921e224ce3aa713
Antoine Lorence <antoine.lorence@insa-rennes.fr>
2014-12-09 14:53:41 +0100

    [About] Fix information in about dialog
    Add licence text

--- GenericInterface/Widgets/AboutDialog.h
+++ GenericInterface/Widgets/AboutDialog.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011-2014 INSA Rennes
+ *
+ * This file is part of DETIQ-T.
+ *
+ * DETIQ-T is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DETIQ-T is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DETIQ-T.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef ABOUTDIALOG_H
 #define ABOUTDIALOG_H
 
